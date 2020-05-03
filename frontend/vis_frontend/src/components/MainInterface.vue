b5977af012df47090e020c9b059eb6e0d5fd9626
ltt <Luottian@163.com>
2020-05-03 23:03:55 +0800

    Update MainInterface.vue

--- frontend/vis_frontend/src/components/MainInterface.vue
+++ frontend/vis_frontend/src/components/MainInterface.vue
@@ -85,6 +85,17 @@
       >
         <worldMap />
       </GridItem>
+	  <GridItem
+              :i="layout[5].i"
+              :x="layout[5].x"
+              :y="layout[5].y"
+              :w="layout[5].w"
+              :h="layout[5].h"
+              :is-resizable="true"
+              :min-w="4"
+      >
+        <Line />
+      </GridItem>
     </GridLayout>
   </v-app>
 </template>
