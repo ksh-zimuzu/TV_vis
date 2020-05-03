8fef46e529bab5dbc177eb6e11ac440d3a28bc35
EchoChou990919 <1505682793@qq.com>
2020-05-03 18:11:50 +0800

    Update worldMap.vue

--- frontend/vis_frontend/src/components/worldMap.vue
+++ frontend/vis_frontend/src/components/worldMap.vue
@@ -6,7 +6,7 @@
 
 <script>
     import echarts from "echarts" //引入组件
-    import '../services/world.js'    //引入组件
+    import '../../node_modules/echarts/map/js/world.js'    //引入组件
 
     export default {
       name: "worldMap",
