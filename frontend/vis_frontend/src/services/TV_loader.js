863124d98ca2547529974268454e16b3f927d4c1
Zhu Jiamin <32239719+KXXH@users.noreply.github.com>
2020-05-07 21:11:11 +0800

    再次尝试修复

--- frontend/vis_frontend/src/services/TV_loader.js
+++ frontend/vis_frontend/src/services/TV_loader.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const requests = axios.create({
-    baseURL: process.env.publicPath
-})
+var baseURL = process.env.BASE_URL;
+
 
 export default {
     fetch_all(tv_name) {
@@ -18,10 +17,10 @@ export default {
         };
     },
     fetch_meta(tv_name) {
-        return requests.get(`/TVs/${tv_name}/meta.json`);
+        return axios.get(`${baseURL}TVs/${tv_name}/meta.json`);
     },
     fetch_word_freq(tv_name) {
-        return requests.get(`/TVs/${tv_name}/word_freq.json`);
+        return axios.get(`${baseURL}TVs/${tv_name}/word_freq.json`);
     },
     fetch_fps(tv_name, meta) {
         var res = meta.episodes.map((ep) => ({
@@ -33,15 +32,15 @@ export default {
     fetch_fp(tv_name, season, episode) {
         var season_str = String(season).padStart(2, "0");
         var episode_str = String(episode).padStart(2, "0");
-        return requests.get(`/TVs/${tv_name}/FP_S${season_str}E${episode_str}.json`);
+        return axios.get(`${baseURL}TVs/${tv_name}/FP_S${season_str}E${episode_str}.json`);
     },
     fetch_actors(tv_name) {
-        return requests.get(`/TVs/${tv_name}/actor_list.json`);
+        return axios.get(`${baseURL}TVs/${tv_name}/actor_list.json`);
     },
     fetch_plot(tv_name, season, episode) {
         var season_str = String(season).padStart(2, "0");
         var episode_str = String(episode).padStart(2, "0");
-        return requests.get(`/TVs/${tv_name}/plot_S${season_str}E${episode_str}.txt`);
+        return axios.get(`${baseURL}TVs/${tv_name}/plot_S${season_str}E${episode_str}.txt`);
     },
     fetch_plots(tv_name, meta) {
         var res = meta.episodes.map((ep) => ({
