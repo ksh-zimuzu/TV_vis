e588e79bb6f9273f9a1865fda204b433069a5876
ltt <Luottian@163.com>
2020-05-10 13:41:04 +0800

    Update Liner.vue

--- frontend/vis_frontend/src/components/Liner.vue
+++ frontend/vis_frontend/src/components/Liner.vue
@@ -7,126 +7,219 @@
 <script>
 import echarts from 'echarts' // 引入组件
 import '../../node_modules/echarts/lib/chart/line'
-import $ from 'jquery'
+import China from '../data/China.json'
+import America from '../data/America.json'
+import English from '../data/English.json'
+import Japan from '../data/Japan.json'
+import Korea from '../data/Korea.json'
 export default {
   name: 'Liner',
-  data () {
-    return {
-      chart: null,
-      place: 'China'
-    }
-  },
   props: {
     width: { type: String, default: '1000px' },
     height: {type: String, default: '500px'}
   },
+  data () {
+    return {
+      chart: null,
+      se: [],
+      nameo: [],
+      da: [],
+      country: 'Korea'
+    }
+  },
   mounted () {
     this.initChart()
+    this.change(this.country)
   },
-  
+  watch: {
+    country: {
+      handler (newVal) {
+        console.log(newVal)
+        this.change(this.country)
+      }
+    }
+  },
   methods: {
-  initChart(){
-    this.chart = echarts.init(this.$refs.myEchart)
-    window.onresize = echarts.init(this.$refs.myEchart).resize
-    var se = []
-    var nameo = []
-    $.ajax('../data/'+this.place+'.json').then((res) =>{
-      alert(res.nameofmovie)
-      console.log(res)
-      res.nameofmovie.forEach((item, index) => {
-        nameo[index] = res.nameofmovie
+    initChart () {
+      this.chart = echarts.init(this.$refs.myEchart)
+      window.onresize = echarts.init(this.$refs.myEchart).resize
+      // 绘制图表
+      var option = {
+      // 标题
+        title: {
+          text: 'tmdb部分电视剧收视率折线图'
+        },
+        tooltip: {
+          trigger: 'axis',
+          // 当鼠标移动到数值时候，在X轴Y轴显示数值
+          axisPointer: {
+            type: 'cross'
+          },
+          triggerOn: 'mousemove',
+          enterable: true
+        },
+        dataZoom: [// 控制缩放
+          {
+            type: 'slider',
+            xAxisIndex: 0,
+            start: 0,
+            end: 100
+          },
+          {
+            type: 'inside',
+            xAxisIndex: 0,
+            start: 0,
+            end: 100
+          },
+          {
+            type: 'slider',
+            yAxisIndex: 0,
+            start: 0,
+            end: 100
+          },
+          {
+            type: 'inside',
+            yAxisIndex: 0,
+            start: 0,
+            end: 100
+          }
+        ],
+        // 工具箱
+        // 保存图片
+        toolbox: {
+          show: true,
+          feature: {
+            saveAsImage: {
+              show: true
+            }
+          }
+        },
+        // 图例-每一条数据的名字
+        legend: {
+          show: false,
+          data: this.nameofmovie
+        },
+        // x轴
+        xAxis: {
+          name: '时间',
+          data: this.da
+        },
+        // y轴没有显式设置，根据值自动生成y轴
+        yAxis: {
+          name: '收视率'
+        },
+        // 数据-data是最终要显示的数据
+        series: this.se
+      }
+      this.chart.setOption(option)
+    },
+    change (country) {
+      var coun = {}
+      if (country === 'China') {
+        coun = China
+      } else if (country === 'America') {
+        coun = America
+      } else if (country === 'English') {
+        coun = English
+      } else if (country === 'Japan') {
+        coun = Japan
+      } else if (country === 'Korea') {
+        coun = Korea
+      }
+      var beginDt = new Date('2019-11-20')
+      console.log(coun)
+      var na = coun.nameofmovie
+      var rat = coun.rating
+      na.forEach((item, index) => {
+        this.nameo.push(na)
         var seri = {
           name: '',
           type: 'line',
           data: []}
         seri.name = item
-        seri.data = res.rating[index]
-        se[index] = seri
+        seri.data = rat[index]
+        this.se.push(seri)
       })
-      })
-    // this.chart.showLoading()
-    // 绘制图表
-    var option = {
+      for (var i = 0; i < coun.rating[0].length; i++) {
+        var d = beginDt.getFullYear() + '-' + (beginDt.getMonth() + 1) + '-' + beginDt.getDate()
+        this.da.push(d)
+        beginDt.setDate(beginDt.getDate() + 1)
+      }
+      var option = {
       // 标题
-      title: {
-        text: 'tmdb部分电视剧收视率折线图'
-      },
-      tooltip: {
-        trigger: 'axis',
-        // 当鼠标移动到数值时候，在X轴Y轴显示数值
-        axisPointer: {
-          type: 'cross'
+        title: {
+          text: 'tmdb部分电视剧收视率折线图'
         },
-        triggerOn: 'mousemove',
-        enterable: true
-      },
-      dataZoom: [// 控制缩放
-        {
-          type: 'slider',
-          xAxisIndex: 0,
-          start: 0,
-          end: 100
+        tooltip: {
+          trigger: 'axis',
+          // 当鼠标移动到数值时候，在X轴Y轴显示数值
+          axisPointer: {
+            type: 'cross'
+          },
+          triggerOn: 'mousemove',
+          enterable: true
         },
-        {
-          type: 'inside',
-          xAxisIndex: 0,
-          start: 0,
-          end: 100
-        },
-        {
-          type: 'slider',
-          yAxisIndex: 0,
-          start: 0,
-          end: 100
-        },
-        {
-          type: 'inside',
-          yAxisIndex: 0,
-          start: 0,
-          end: 100
-        }
-      ],
-      // 工具箱
-      // 保存图片
-      toolbox: {
-        show: true,
-        feature: {
-          saveAsImage: {
-            show: true
+        dataZoom: [// 控制缩放
+          {
+            type: 'slider',
+            xAxisIndex: 0,
+            start: 0,
+            end: 100
+          },
+          {
+            type: 'inside',
+            xAxisIndex: 0,
+            start: 0,
+            end: 100
+          },
+          {
+            type: 'slider',
+            yAxisIndex: 0,
+            start: 0,
+            end: 100
+          },
+          {
+            type: 'inside',
+            yAxisIndex: 0,
+            start: 0,
+            end: 100
           }
-        }
-      },
-      // 图例-每一条数据的名字
-      legend: {
-        data: nameo
-      },
-      // x轴
-      xAxis: {
-        type: 'time',
-        name: '时间',
-        minInterval: 1,
-        min: (new Date('2019-11-20')),
-        max: (new Date('2020-04-26'))
-      },
-      // y轴没有显式设置，根据值自动生成y轴
-      yAxis: {
-        name: '收视率'
-      },
-      // 数据-data是最终要显示的数据
-      series: se
+        ],
+        // 工具箱
+        // 保存图片
+        toolbox: {
+          show: true,
+          feature: {
+            saveAsImage: {
+              show: true
+            }
+          }
+        },
+        // 图例-每一条数据的名字
+        legend: {
+          show: false,
+          data: this.nameofmovie
+        },
+        // x轴
+        xAxis: {
+          name: '时间',
+          data: this.da
+        },
+        // y轴没有显式设置，根据值自动生成y轴
+        yAxis: {
+          name: '收视率'
+        },
+        // 数据-data是最终要显示的数据
+        series: this.se
+      }
+      this.chart.setOption(option)
+      this.chart.on('click', (param) => {
+        console.log(param)
+      // var name = param.name
+        window.location.href = '/HelloWorld'
+      })
     }
-    this.chart.hideLoading()
-    this.chart.setOption(option)
-    this.chart.on('click', (param) => {
-      var name = param.seriesName
-      window.location.href = '/HelloWorld'+name
-    })
-    
   }
-  }
+
 }
 </script>
-
-<style scoped>
-
-</style>
