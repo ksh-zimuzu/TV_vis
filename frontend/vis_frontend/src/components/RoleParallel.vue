bf6af4e38ec1be790b29391199c5e1543501d819
Zhu Jiamin <32239719+KXXH@users.noreply.github.com>
2020-05-11 23:14:38 +0800

    加入演员出现次数折线图（初步）

--- frontend/vis_frontend/src/components/RoleParallel.vue
+++ frontend/vis_frontend/src/components/RoleParallel.vue
@@ -4,14 +4,100 @@
 
 <script>
 import echarts from "echarts";
+import _ from "lodash";
 
 export default {
   name: "RoleParallel",
   mounted: function() {
     this.chart = echarts.init(this.$el);
+    this.$EventBus.$on("actor-focus", this.onFocus);
   },
   data: () => ({
     chart: undefined
-  })
+  }),
+  props: {
+    chartData: {
+      type: Array,
+      required: true
+    },
+    parallelAxis: {
+      type: Array,
+      required: true
+    },
+    colors: {
+      default: () => [
+        "#E3BA22",
+        "#e58429",
+        "#bd2d28",
+        "#8e6c8a",
+        "#6b99a1",
+        "#42a5b3",
+        "#0f8c79"
+      ]
+    }
+  },
+  computed: {
+    option: function() {
+      return {
+        color: this.colors,
+        //parallelAxis: this.parallelAxis,
+        series: this.chartData,
+        /*
+        parallel: {
+          // 这是『坐标系』的定义
+          left: "5%", // 平行坐标系的位置设置
+          right: "13%",
+          bottom: "10%",
+          top: "20%",
+          parallelAxisDefault: {
+            // 『坐标轴』的公有属性可以配置在这里避免重复书写
+            type: "value",
+            nameLocation: "end",
+            nameGap: 20
+          }
+        },
+        */
+        xAxis: {
+          type: "category"
+          //data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: "value"
+        },
+        tooltip: {
+          show: true
+        }
+        /*
+        legend: {
+          show: true
+        }
+        */
+      };
+    }
+  },
+  watch: {
+    option: function() {
+      this.chart.setOption(this.option);
+    }
+  },
+  methods: {
+    onFocus: function(msg) {
+      var character = msg.character;
+      if (_.isArray(character)) {
+        character = character.join("-");
+      }
+      for (var value of this.chartData) {
+        var roleIndex = value.data.findIndex(item => item.name == character);
+        if (roleIndex != -1) {
+          this.chart.dispatchAction({
+            type: "highlight",
+            seriesName: value.name,
+            dataIndex: roleIndex
+          });
+          break;
+        }
+      }
+    }
+  }
 };
 </script>
--- frontend/vis_frontend/src/components/RoleParallelBox.vue
+++ frontend/vis_frontend/src/components/RoleParallelBox.vue
@@ -1,6 +1,6 @@
 <template>
-  <smart-widget>
-    <RoleParallel />
+  <smart-widget simple>
+    <RoleParallel :chartData="chartData" :parallelAxis="parallelAxis" ref="roleparallel" />
   </smart-widget>
 </template>
 
@@ -15,23 +15,129 @@ export default {
     RoleParallel
   },
   data: () => ({
-    nodes: undefined
+    nodes: undefined,
+    highlightGroup: -1,
+    highlightIndex: -1,
+    highlightName: "",
+    colors: [
+      "#E3BA22",
+      "#e58429",
+      "#bd2d28",
+      "#8e6c8a",
+      "#6b99a1",
+      "#42a5b3",
+      "#0f8c79"
+    ]
   }),
   created: function() {
     this.$EventBus.$on("nodes-ready", msg => (this.nodes = msg));
   },
   computed: {
-    data: function() {
-      var groups = _.groupBy(this.nodes, t => t.category);
-      return _.keys(groups).map(key => {
-        var matrix = groups[key].map(t => t.map(x => x.value));
+    chartData: function() {
+      if (this.nodes == undefined) {
+        return [];
+      }
+      var groups = this.nodes.map(ep => {
+        var res = _.groupBy(ep, t => t.category);
+        _.keys(res).map(
+          key =>
+            (res[key] = res[key].map(t => ({
+              value: t.value,
+              name: t.name
+            })))
+        );
+        return res;
+      });
+      var matrix = _.assignWith({}, ...groups, (objValue, srcValue) => {
+        if (_.isArray(objValue)) {
+          objValue.push(srcValue);
+          return objValue;
+        } else {
+          return [srcValue];
+        }
+      });
+      var transposedMatrix = _.keys(matrix)
+        .sort()
+        .map(key => numbers.matrix.transpose(matrix[key]));
+      return _.flatten(
+        transposedMatrix.map((x, index) => {
+          return x.map(y => {
+            var highlight =
+              this.highlightName.length == 0 || this.highlightName == y[0].name;
+            return {
+              name: y[0].name,
+              data: y.map(z => z.value),
+              type: "line",
+              category: `${index ? index + 1 : "单"}人关系`,
+              lineStyle: {
+                color: highlight
+                  ? this.colors[index % this.colors.length]
+                  : "#e7e6e6",
+                width: highlight && this.highlightName.length ? 2 : 0.5
+              },
+              symbol: "none",
+              smooth: true,
+              z: highlight ? 9999 : index
+            };
+          });
+        })
+      );
+      /*
         return {
-          name: key,
-          type: "parallel",
-          data: numbers.matrix.transpose(matrix)
+          name: `${index ? index + 1 : "单"}人关系`,
+          type: "line",
+          data: x.map((y, i) => {
+            return {
+              name: y[0].name,
+              value: y.map(z => z.value),
+            };
+          }),
+          smooth: true,
+          z: this.highlightGroup == index ? 9999 : 1
         };
       });
+      */
+
+      //      return matrix;
+    },
+    parallelAxis: function() {
+      if (this.nodes == undefined) {
+        return [];
+      }
+      return this.nodes.map((t, index) => ({
+        dim: index,
+        name: `第${index + 1}集`
+      }));
     }
+  },
+  methods: {
+    resizeEvent: function() {
+      this.resizeFunc();
+    },
+    onFocus: function(msg) {
+      var character = msg.character;
+      if (_.isArray(character)) {
+        character = character.join("-");
+      }
+      this.highlightName = character;
+      /*
+      for (var i = 0; i < this.chartData.length; i++) {
+        var value = this.chartData[i];
+        var roleIndex = value.data.findIndex(item => item.name == character);
+        if (roleIndex != -1) {
+          this.highlightGroup = i;
+          this.highlightIndex = roleIndex;
+          break;
+        }
+      }
+      */
+    }
+  },
+  mounted: function() {
+    this.resizeFunc = _.debounce(this.$refs.roleparallel.chart.resize, 500);
+    this.resizeFunc();
+    this.$parent.$on("resized", this.resizeEvent);
+    this.$EventBus.$on("actor-focus", this.onFocus);
   }
 };
 </script>
