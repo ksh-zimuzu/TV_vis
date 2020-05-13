309c5aca426e7d5602dff8f428082b5c9750f31d
ltt <Luottian@163.com>
2020-05-13 22:44:33 +0800

    异常处理

--- frontend/vis_frontend/src/components/PlayerChartBox.vue
+++ frontend/vis_frontend/src/components/PlayerChartBox.vue
@@ -8,7 +8,6 @@
 import PlayerChart from "./PlayerChart";
 import _ from "lodash";
 import se from "../services/searchtv"
-//import test from "../../public/data _id/2084790.json"
 
 export default {
   name: "PlayerChartBox",
@@ -18,7 +17,7 @@ export default {
   data: function() {
     return {
       chartData: {
-        西部世界: {
+        示例数据1: {
           name: "Clabs",
           present: [
             7,
@@ -46,14 +45,14 @@ export default {
             豆瓣: 7
           }
         },
-        绝命毒师: {
+        示例数据2: {
           name: "John",
           present: [5, 1, 1, 4, 3, 1, 1, 5, 5, 11],
           rating: {
             豆瓣: 8.5
           }
         },
-        权力的游戏: {
+        示例数据3: {
           name: "Bob",
           present: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 3, 2, 2, 3, 2, 2, 6, 3],
           rating: {
@@ -82,9 +81,16 @@ export default {
     cha: async function(da){
       for (var b in da){
         var p = await se.pop(b);
-        da[b].rating = {
+        if (p == undefined){
+          da[b].rating = {
+          豆瓣: 0
+        };
+        } else{
+          da[b].rating = {
           豆瓣: p
         };
+        }
+        
       }
       this.chartData = da;
       console.log("chartData")
@@ -99,12 +105,16 @@ export default {
       console.log("result:");
       console.log(res.data);
       this.da = res.data;
-      })
+      }).catch((err) =>{
+        console.log(err);
+        alert("该演员生涯分析缺失，制作组正在征集数据噢awa")
+      });
     console.log("da")
     console.log(this.da)
     for(var a in this.da){
       delete this.da[a].time;
     }
+    console.log("da2:")
     console.log(this.da);
     await this.cha(this.da);
     
