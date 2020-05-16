b984ba28100e8f1d35c59bf7eff99fe10be0aea6
Zhu Jiamin <32239719+KXXH@users.noreply.github.com>
2020-05-16 10:58:51 +0800

    Update PlayerAvatarBox.vue

--- frontend/vis_frontend/src/components/PlayerAvatarBox.vue
+++ frontend/vis_frontend/src/components/PlayerAvatarBox.vue
@@ -34,7 +34,8 @@ export default {
     }
   },
   mounted: function() {
-    this.resizeFunc = _.debounce(this.$refs.avatarChart.chart.resize, 500);
+    this.$refs.avatarChart.resize();
+    this.resizeFunc = _.debounce(this.$refs.avatarChart.resize, 500);
     this.resizeFunc();
     this.$parent.$on("resized", this.resizeEvent); //接收外层resize事件
     this.$parent.$on("container-resized", this.resizeEvent);
